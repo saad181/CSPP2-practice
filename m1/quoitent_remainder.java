7d7898449d1b877dc29713a3a2a91043f6baa05e
saad181 <saadahmed181@msitprogram.net>
2018-08-27 17:50:35 +0530

    quotient_remainder

---
+++ m1/quoitent_remainder.java
@@ -0,0 +1,17 @@
+ class remainder
+{
+    public static void main (String args[])
+	{
+		int a = 10;
+		int b = 5;
+		int quo;
+		int rem;
+		quo = a/b;
+		rem = a%b;
+		System.out.println(" the quotient is :" +quo);
+		System.out.println("the remainderis :" +rem);
+
+
+
+    }
+}
