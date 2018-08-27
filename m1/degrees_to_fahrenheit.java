8a0b6ed5643ddfd8c77a222700cfe89ff488309b
saad181 <saadahmed181@msitprogram.net>
2018-08-27 18:07:29 +0530

    degrees_to_fahrenheit

---
+++ m1/degrees_to_fahrenheit.java
@@ -0,0 +1,8 @@
+public class degrees_to_fahrenheit{
+	public static void main(String args[])
+	{
+		double x = 30;
+		double conv = x*1.8+32;
+		System.out.println(" the fahrenheit temperature is :" +conv);
+	}
+}
