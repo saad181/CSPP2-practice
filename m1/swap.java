9be1669bf53e4c0a8ea372bc464a69f91c8b144a
saad181 <saadahmed181@msitprogram.net>
2018-08-27 17:34:52 +0530

    swap.java

---
+++ m1/swap.java
@@ -0,0 +1,14 @@
+public class swap
+{
+  public static void main(String[] args)
+  {
+  	int a = 5;
+  	int b = 3;
+  	int temp;
+  	temp = a;
+  	a = b;
+  	b = temp;
+    System.out.println("before swapping" +a );
+    System.out.println("after swapping" +b );
+  }
+}
