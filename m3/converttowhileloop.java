0cfac05453643b8a7912415bc5db7c6a11957f51
saad181 <saadahmed181@msitprogram.net>
2018-08-29 13:45:58 +0530

    converttowhileloop

---
+++ m3/converttowhileloop.java
@@ -0,0 +1,13 @@
+public class converttowhileloop
+{
+	public static void main(String[] args) {
+		int i=0;
+		while(i<= 8)
+		{
+			i = i+2;
+			System.out.println(i);
+
+		}
+		System.out.println("GoodBye!");
+	}
+}
